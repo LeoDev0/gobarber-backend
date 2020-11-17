d467c9af474a41f6d636c0680844c1066a62533b
Leonardo T. Fernandes <leo.80@protonmail.com>
2020-11-17 10:06:16 -0300

    upload de imagens

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -31,5 +37,34 @@ usersRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: err.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        try {
+            const updateUserAvatar = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFilename: request.file.filename,
+            });
+
+            const userWithoutPassword = {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                avatar: request.file.filename,
+                created_at: user.created_at,
+                updated_at: user.updated_at,
+            };
+
+            return response.json(userWithoutPassword);
+        } catch (error) {
+            return response.status(400).json({ error: error.message });
+        }
+    },
+);
 
 export default usersRouter;
