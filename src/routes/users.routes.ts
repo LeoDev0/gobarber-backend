e60229481f9c4b77876f76a301bc048ff9eb9864
Leonardo T. Fernandes <leo.80@protonmail.com>
2020-11-18 07:27:44 -0300

    tratando erros/exceções

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,31 +11,27 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-    try {
-        const { name, email, password } = request.body;
+    const { name, email, password } = request.body;
 
-        const createUser = new CreateUserService();
+    const createUser = new CreateUserService();
 
-        const user = await createUser.execute({
-            name,
-            email,
-            password,
-        });
+    const user = await createUser.execute({
+        name,
+        email,
+        password,
+    });
 
-        // delete user.password;  // TypeScript sofreu uma alteração onde agora só é possível deletar propriedades de objetos se ela for opcional, any, never ou possuir undefined no tipo
+    // delete user.password;  // TypeScript sofreu uma alteração onde agora só é possível deletar propriedades de objetos se ela for opcional, any, never ou possuir undefined no tipo
 
-        const userWithoutPassword = {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            created_at: user.created_at,
-            updated_at: user.updated_at,
-        };
+    const userWithoutPassword = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+    };
 
-        return response.json(userWithoutPassword);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
+    return response.json(userWithoutPassword);
 });
 
 usersRouter.patch(
@@ -43,27 +39,23 @@ usersRouter.patch(
     ensureAuthenticated,
     upload.single('avatar'),
     async (request, response) => {
-        try {
-            const updateUserAvatar = new UpdateUserAvatarService();
+        const updateUserAvatar = new UpdateUserAvatarService();
 
-            const user = await updateUserAvatar.execute({
-                user_id: request.user.id,
-                avatarFilename: request.file.filename,
-            });
+        const user = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFilename: request.file.filename,
+        });
 
-            const userWithoutPassword = {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                avatar: request.file.filename,
-                created_at: user.created_at,
-                updated_at: user.updated_at,
-            };
+        const userWithoutPassword = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            avatar: request.file.filename,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+        };
 
-            return response.json(userWithoutPassword);
-        } catch (error) {
-            return response.status(400).json({ error: error.message });
-        }
+        return response.json(userWithoutPassword);
     },
 );
 
